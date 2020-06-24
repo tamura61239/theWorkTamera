e9a953aa3b8f337c540670c4de0e7f2f17ab569b
TAName <oarmiukr@ezweb.ne.jp>
2020-06-24 17:17:51 +0900

    追加

--- gameLib(ver 1.2)/Source/swirl_particle_compute.hlsl
+++ gameLib(ver 1.2)/Source/swirl_particle_compute.hlsl
@@ -13,28 +13,25 @@ void main( uint3 DTid : SV_DispatchThreadID )
 	float3 velocity = asfloat(rwbParticle.Load3(bufferIndex + 16));
 	float4 color = asfloat(rwbParticle.Load4(bufferIndex + 28));
 
-	//velocity.x = sin(velocity.y);
-	//velocity.z = cos(velocity.y);
-	//float3 vec = normalize(position.xyz - centerPosition)*0.001f;
-	//velocity.x -= vec.x;
-	//velocity.z -= vec.z;
-	//position.x += velocity.x*0.001f;
-	//position.z += velocity.z*0.001f;
-	//velocity.y += 0.01745f;
+	//中心へのベクトルを求める
 	float3 vec = centerPosition - position.xyz;
 	float leng = length(vec);
 	vec = normalize(vec);
 	float3 up = float3(0, 1, 0);
 	//leng = 1 - 1 / leng;
+	//移動方向を求める
 	velocity = cross(up, vec);
 	velocity += vec;
 	velocity = normalize(velocity);
-	velocity *= 0.01f/* * leng*/;
+	//移動量を算出する
+	velocity *= 0.01f;
 	position.xyz += velocity;
+	//中心からの距離が一定距離より短くなったら初期化する
 	//step(a,x):(x >= a) ? 1 : 0 (xがa以上の場合は1, それ以外は0を返します)
 	uint set = step(0.01f, leng);
 	position = lerp(asfloat(resetParticle.Load4(bufferIndex + 0)), position, set);
 	color = lerp(asfloat(resetParticle.Load4(bufferIndex + 28)), color, set);
+	//色や不透明度を求める
 	float l = leng - 4.f;
 	l = max(l,0);
 	l = min(l, 1);
