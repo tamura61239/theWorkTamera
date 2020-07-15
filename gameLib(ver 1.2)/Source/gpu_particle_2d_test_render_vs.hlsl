9a71659f403c8d094f735ffdce6767ee93975086
TAName <oarmiukr@ezweb.ne.jp>
2020-07-15 22:13:09 +0900

    2d_particle delete

--- gameLib(ver 1.2)/Source/gpu_particle_2d_test_render_vs.hlsl
+++
@@ -1,8 +0,0 @@
-#include"gpu_particle_test_render.hlsli"
-VS_OUT main(float4 position : POSITION, float3 velocity : VELOCITY, float4 color : COLOR)
-{
-	VS_OUT vout;
-	vout.position = position;
-	vout.color = color;
-	return vout;
-}
